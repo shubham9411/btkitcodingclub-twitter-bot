5d65325f10abb7df29f432b204801eed0387acb7
Shubham Pandey <shubham9411@gmail.com>
2018-02-21 00:52:51 +0530

    added config from process env

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret:  process.env.consumer_secret,
+  access_token:  process.env.access_token,
+  access_token_secret:  process.env.access_token_secret
+}
